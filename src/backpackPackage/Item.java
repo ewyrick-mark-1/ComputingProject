74431cc36952c03d1f7ba32fc5bc0d8c0fd5547e
pleasedaspie <julianvmansfield@gmail.com>
2024-12-16 22:46:07 -0500

    comments

--- src/backpackPackage/Item.java
+++ src/backpackPackage/Item.java
@@ -1,57 +1,65 @@
 package backpackPackage;
 
-public class Item{
-  private String name;
-  private int value;
-  private int regen;
-  private int indentifier;
+public class Item {
+  private String name;    // Name of the item
+  private int value;      // Value of the item
+  private int regen;      // Regeneration value (for potions)
+  private int indentifier; // Identifier to distinguish items
 
+  // Default constructor: Initializes item with default values
   public Item(){
     name = "default";
     value = 0;
     regen = 0;
     indentifier = 0;
   }
+
+  // Constructor: Initializes item based on identifier (a)
   public Item(int a){
     if(a == 1){
       name = "blueberies";
       value = 5;
-      
-    }else if(a == 2){
+    } else if(a == 2){
       name = "stones";
       value = 4;
-    }else if(a == 3){
+    } else if(a == 3){
       name = "twigs";
       value = 3;
-    }else if(a == 4){
+    } else if(a == 4){
         name = "Basic Health potion";
         value = 20;
-        regen = 50;
-     }else if(a == 5){
-          name = "Strong Health potion";
-          value = 20;
-          regen = 100;
-     }
-    indentifier = a;
+        regen = 50;  // Health regeneration for basic potion
+    } else if(a == 5){
+        name = "Strong Health potion";
+        value = 20;
+        regen = 100; // Health regeneration for strong potion
+    }
+    indentifier = a;  // Set item identifier
   }
+
+  // Constructor: Initializes item with custom name and value
   public Item(String a, int b){
     name = a;
     value = b;
   }
+
+  // Getter for item name
   public String getName(){
-		  return name;
+    return name;
   }
+
+  // Getter for item value
   public int getValue(){
     return value;
   }
+
+  // Getter for regeneration value
   public int getRegen(){
-	  return regen;
+    return regen;
   }
+
+  // Getter for item identifier
   public int getIdentifier() {  
-	  
-	  return indentifier;
-	  }
-  
+    return indentifier;
   }
-
-
+}
